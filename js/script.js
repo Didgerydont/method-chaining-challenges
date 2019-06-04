11918aec9b5a080175146b682d32723aa33e9d32
Will Prendergast <prendygast@hotmail.com>
2019-06-04 21:38:39 +0000

    Challenge 1&2 complete

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
- //
- // add your jQuery code here
+
     $("#button1").mouseenter(function(){
 		$(this).removeClass("makeRed").addClass("makeBorder");
 	});
 
 	$("#button1").mouseleave(function(){
 		$("#button1").removeClass("makeBorder").addClass("makeRed");
+	});
+	
+	$("button").click(function(){
+	    $("#para1").slideToggle("fast");
 	});
 });
